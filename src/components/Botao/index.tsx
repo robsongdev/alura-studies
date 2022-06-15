1870a3ecc39496c7c8b13b03e662e74b0a483f66
robsongdev <robsong369@gmail.com>
2022-06-15 11:26:57 -0300

    create Botao component

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Botao extends React.Component{
+    render():React.ReactNode {
+        return (
+            <button>
+                Botão
+            </button>
+        )
+    }
+}
