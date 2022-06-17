ec41b21a9042d4a9bf1f7bcf89e50f3092a16095
robsongdev <robsong369@gmail.com>
2022-06-17 10:40:50 -0300

    Add styles

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Botao from "../Botao";
+import style from './Formulario.module.scss';
 
 export default class Formulario extends React.Component{
     render(): React.ReactNode {
         return (
-            <form>
-                <div>
+            <form className={style.novaTarefa}>
+                <div className={style.inputContainer}>
                     <label htmlFor="tarefa">Adicione um novo estudo</label>
                     <input type="text" name="tarefa" id="tarefa" placeholder="O que você quer estudar" required/>
                 </div>
-                <div>
+                <div className={style.inputContainer}>
                     <label htmlFor="tempo">Tempo</label>
                     <input type="time" step="1" name="tempo" min="00:00:00" max="01:30:00" required/>
                 </div>
