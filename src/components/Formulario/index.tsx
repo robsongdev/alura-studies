daaa70529a9f76cf03d41275b96f61413b8488e4
robsongdev <robsong369@gmail.com>
2022-06-20 18:35:06 -0300

    add components created

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -15,7 +15,9 @@ export default class Formulario extends React.Component{
                     <input type="time" step="1" name="tempo" min="00:00:00" max="01:30:00" required/>
                 </div>
                 
-                <Botao/>
+                <Botao>
+                    adicionar
+                </Botao>
             </form>
         )
     }
